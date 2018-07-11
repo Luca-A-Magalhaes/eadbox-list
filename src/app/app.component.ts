5fa93b14f6a0b9a71972a268876eb78aee026421
Luca A Magalhaes <luca.a.magalhaes@gmail.com>
2018-07-11 00:21:24 -0300

    Adicionando requisicao para api com proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  restItems: any;
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    this.getRestItems();
+  }
+
+  getRestItems(): void{
+    this.appService.getAll().subscribe(restItems => {
+      this.restItems = restItems;
+      console.log(this.restItems);
+    })
+  }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,27 @@
+import { Injectable  } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class AppService{
+
+  protected url : string = '/api/courses';
+
+  constructor(private http: HttpClient) {}
+
+  // Rest Items Service: Read all REST Items
+  getAll() {
+    return this.http
+      .get<any[]>(this.url, httpOptions)
+      .pipe(map((obj, i) => ({
+          url: "http://escola-do-luca.eadbox.com/ng/student/courses/" + obj[i].course_slug,
+          titulo: obj[i].title,
+          descricao: obj[i].description
+      })));
+  }
+
+}
